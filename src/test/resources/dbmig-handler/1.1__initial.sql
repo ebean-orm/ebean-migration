ac6ac80dac21a4f069c149ba41ad48885214d5e0
Roland Praml <praml@foconis.de>
2018-05-29 19:29:53 +0200

    Test with semicolon

--- src/test/resources/dbmig-handler/1.1__initial.sql
+++ src/test/resources/dbmig-handler/1.1__initial.sql
@@ -5,13 +5,16 @@ runStatic:io.ebean.migration.MigrationRunnerTest.myMigration("Hello\nWorld");
 
 runStatic:io.ebean.migration.MigrationRunnerTest.createTable("m2", "id integer", "acol varchar(20)");
 
+-- have to use delimiter here, because the statement contains semicolons
+delimiter $$
 runStatic: io.ebean.migration.MigrationRunnerTest.demo(
-    "This\tis\ta\tString\targ", 
+    "This\tis\ta\tString\targ;", 
     ["ListElem1", "ListElem2", 3, 4, null],
     2012-01-01,
     3.45,
     2016-02-03T12:10:55+01:00
     );
+$$
 
 -- do logging
     logger:Hello This is line 15 from the 1.1__initial script;
