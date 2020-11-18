34f956024441e916e7b6a35baefc5d81b3ca1cc3
rob bygrave <robin.bygrave@gmail.com>
2020-11-18 20:48:09 +1300

    #79 - Add ebean-migration-auto for minimising the dependency to ebean-api

--- ebean-migration-auto/src/main/java/io/ebean/migration/auto/AutoMigrationRunner.java
+++ ebean-migration-auto/src/main/java/io/ebean/migration/auto/AutoMigrationRunner.java
@@ -9,7 +9,19 @@ import java.util.Properties;
 public interface AutoMigrationRunner {
 
   /**
+   * Set the name of the database the migration is run for.
+   * <p>
+   * This name can be used when loading properties like:
+   * <code>ebean.${name}.migration.migrationPath</code>
+   */
+  void setName(String name);
+
+  /**
    * Set a default DB schema to use.
+   * <p>
+   * This is mostly for Postgres use where the dbSchema matches the DB username. In this case
+   * we don't set the current schema as that can mess up the Postgres search path.
+   * </p>
    */
   void setDefaultDbSchema(String defaultDbSchema);
 
