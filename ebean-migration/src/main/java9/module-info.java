b3a7e6f0341a0148f363cf5c98f4871be4bd0be6
Rob Bygrave <robin.bygrave@gmail.com>
2021-09-16 11:46:49 +1200

    Fix module-info requires section

--- ebean-migration/src/main/java9/module-info.java
+++ ebean-migration/src/main/java9/module-info.java
@@ -1,11 +1,13 @@
 module io.ebean.migration {
 
-  requires transitive java.sql;
-  requires io.ebean.migration.auto;
-  requires org.slf4j;
-
   exports io.ebean.migration;
   exports io.ebean.migration.runner;
+
+  requires transitive java.sql;
+  requires transitive org.slf4j;
+  requires transitive io.avaje.classpath.scanner;
+  requires transitive io.ebean.ddl.runner;
+  requires io.ebean.migration.auto;
 
   provides io.ebean.migration.auto.AutoMigrationRunner with io.ebean.migration.AutoRunner;
 }
