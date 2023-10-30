c38fc9b73c6e33bb3ca3fcb0e55582ed9a325641
Rob Bygrave <robin.bygrave@gmail.com>
2023-10-30 23:49:09 +1300

    Test native-image with Process API

--- test-native-image/src/test/java/org/example/StartPostgresContainerTest.java
+++ test-native-image/src/test/java/org/example/StartPostgresContainerTest.java
@@ -3,6 +3,12 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import io.ebean.test.containers.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.List;
+
 class StartPostgresContainerTest {
 
   @Test
@@ -12,5 +18,21 @@ class StartPostgresContainerTest {
       .dbName("mig")
       .build()
       .start();
+  }
+
+  @Test
+  void run() throws Exception {
+    File out = new File("out");
+
+    Process process = new ProcessBuilder()
+      .command("./target/test-native-image")
+      .redirectErrorStream(true)
+      .redirectOutput(ProcessBuilder.Redirect.to(out))
+      .start();
+
+    int code = process.waitFor();
+    System.out.println("exit " + code);
+
+    Files.lines(out.toPath()).forEach(System.out::println);
   }
 }
