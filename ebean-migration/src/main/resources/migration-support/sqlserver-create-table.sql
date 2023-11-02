f9422359caaeb73ee033b0df5477d857cdac996c
robin.bygrave <robin.bygrave@eroad.com>
2023-11-02 17:52:15 +1300

    Change the column order only for create table

--- ebean-migration/src/main/resources/migration-support/sqlserver-create-table.sql
+++ ebean-migration/src/main/resources/migration-support/sqlserver-create-table.sql
@@ -1,10 +1,10 @@
 create table ${table} (
   id                           integer not null,
+  mchecksum                    integer not null,
   mtype                        varchar(1) not null,
-  mstatus                      varchar(10) not null,
   mversion                     varchar(150) not null,
   mcomment                     varchar(150) not null,
-  mchecksum                    integer not null,
+  mstatus                      varchar(10) not null,
   run_on                       datetime2 not null,
   run_by                       varchar(30) not null,
   run_time                     integer not null,
